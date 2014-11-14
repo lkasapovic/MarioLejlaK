cccb9cf7d9750a291773ab8200246bd2d3afa10c
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2014-11-14 09:15:54 -0800

    ttass3lkjhhuiiqkjwed

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -1,28 +1,28 @@
 game.resources = [
+    /* Graphics. 
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},,
+     */
+    {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    /* Atlases 
+     * @example
+     * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+     */
 
-	/* Graphics. 
-	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
-	 */
+    /* Maps. 
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
+    {name: "LejlaLevel01", type: "tmx", src: "data/map/level04.tmx"}
 
-	/* Atlases 
-	 * @example
-	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
-	 */
-		
-	/* Maps. 
-	 * @example
-	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
-	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
- 	 */
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
 
-	/* Background music. 
-	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
-	 */	
-
-	/* Sound effects. 
-	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
-	 */
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
 ];
