6b154dea8a40aafcb708b5f36cfbe10b60240540
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2014-11-20 09:11:12 -0800

    no message

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -37,6 +37,8 @@ var game = {
     "loaded": function() {
         me.pool.register("mario", game.PlayerEntity, true);
 
+        me.pool.register("levelTrigger", game.LevelTrigger);
+
         me.state.set(me.state.MENU, new game.TitleScreen());
         me.state.set(me.state.PLAY, new game.PlayScreen());
 
