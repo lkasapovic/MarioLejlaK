4d9ac0125ffb46bf80602229c97866985605c58f
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2014-12-02 09:14:31 -0800

    vid 16

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -6,6 +6,7 @@ game.resources = [
     {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
     {name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png"},
     {name: "mario", type: "image", src: "data/img/player1.png"},
+    {name: "title-screen", type: "image", src: "data/img/title-screen.png"},
     /* Atlases 
      * @example
      * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,7 +3,16 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		; // TODO
+		var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
+                me.game.world.addChild(titleImage, 1);
+                me.input.bindKey(me.input.KEY.ENTER, "start");
+                
+                
+                this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge){
+                    if(action === "start") {
+                        me.state.change(me.state.PLAY);
+                    }
+                });
 	},
 	
 	
